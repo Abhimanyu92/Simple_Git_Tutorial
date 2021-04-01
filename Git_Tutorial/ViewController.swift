f3adca876047f45baec529f428990bf53199ad12
Abhimanyu92 <abhi.bhat92@gmail.com>
2021-04-01 12:50:47 +0530

    Added message for test

---
+++ Git_Tutorial/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Git_Tutorial
+//
+//  Created by Abhimanyu Bhatnagar on 01/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let message = "Hello Git"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print(message)
+    }
+
+
+}
+
